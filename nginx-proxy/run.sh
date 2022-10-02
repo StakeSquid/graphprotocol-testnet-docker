906102a24759e9cee6f86809fb2a7ab61ade2ad4
trader-payne <baboialex95@gmail.com>
2022-10-02 14:05:04 +0300

    fix

--- nginx-proxy/run.sh
+++ nginx-proxy/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 mkdir /etc/nginx/htpasswd/
-echo -n $HTTP_AUTH_LOGIN:$(openssl passwd -apr1 $HTTP_AUTH_PASSWORD) >> /etc/nginx/htpasswd/$PROMETHEUS_HOST
+echo -n $HTTP_AUTH_LOGIN:$(openssl passwd -apr1 $HTTP_AUTH_PASSWORD) > /etc/nginx/htpasswd/$PROMETHEUS_HOST
 nginx -g "daemon off;"
