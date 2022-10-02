ba946aa2b68332cc12244307057683491e6b37fa
trader-payne <baboialex95@gmail.com>
2022-10-02 21:39:01 +0300

    fix nginx ssl

--- nginx-proxy/run.sh
+++ nginx-proxy/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+rm -rf /etc/nginx/htpasswd/
 mkdir /etc/nginx/htpasswd/
 echo -n $HTTP_AUTH_LOGIN:$(openssl passwd -apr1 $HTTP_AUTH_PASSWORD) > /etc/nginx/htpasswd/$PROMETHEUS_HOST
 nginx -g "daemon off;"
